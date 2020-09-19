4a062b463bdf7b4fccca04ec39528417c6df28af
Aly Khalifa <khalifas@mcmaster.ca>
2020-09-19 15:44:54 -0400

    added figures

--- demo.py
+++ demo.py
@@ -9,5 +9,11 @@ app = ScratchDetector()
 #load weights from the most recent training checkpoint
 app.load_most_recent_model()
 
+
+data = ScratchDataset()
+data.load_dataset()
+data.display_random_patches(128)
+
 #display segmentaiton result
-mask = app.display_predicted_mask('frames/mlts_056.png', threshold = 0.7)
+mask = app.display_predicted_mask('frames/mlts_025.png', threshold = 0.7)
+
